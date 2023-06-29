107b82e1e317df5a1535e34f46650eb87d2e52bc
tbello <bello4aus@gmail.com>
2023-06-29 19:16:41 +0100

    - style authentication pages

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,23 @@
+import AuthLayout from "../layouts/authLayout"
+
+
 const Login = () => {
     return(
-        <h1 className="underline"> Login page </h1>
+        <AuthLayout>
+            <h1> Hello Again </h1>
+            <h2> Enter your login details below </h2>
+
+            <form className="flex flex-col">
+                <div> Username </div>
+                <input />
+
+                <div> Password </div>
+                <input />
+
+                <button> Login </button>
+            </form>
+            Don't have an account ? <a href={`/register`}> Sign Up </a>
+        </AuthLayout>
     )
 }
 
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,28 @@
+import AuthLayout from "../layouts/authLayout"
+
 const Register = () => {
     return(
-        <h1 className="underline"> Register page </h1>
+        <AuthLayout>
+            <h1> Create your account </h1>
+            <h2> Enter your personal informtion</h2>
+
+            <form className="flex flex-col">
+                <div> Username </div>
+                <input />
+
+                <div> Password </div>
+                <input />
+
+                <div> Confirm Password </div>
+                <input />
+
+                <button> Sign Up </button>
+            </form>
+
+            <span>
+                Already have an account ? <a href={`/login`}> Login </a>
+            </span>
+        </AuthLayout>
     )
 }
 

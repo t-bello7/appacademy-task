c26727fe38df7d58b25d992cd144c624bb218bfc
tbello <bello4aus@gmail.com>
2023-06-30 22:24:29 +0100

    fix merge issue

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,15 +6,10 @@ const Home = () => {
 
     return(
         <div className="w-[90%] h-screen container mx-auto">
-<<<<<<< HEAD
-            <div className="rounded-lg px-6 py-8 mt-[10vh] shadow-xl bg-white dark:bg-black">
-                <h1 className="underline"> Home page </h1>
-=======
             <div className="rounded-lg px-6 py-8 mt-[10vh] shadow-xl bg-white">
                 <Header />
                 <TodoForm />
                 <TodoContainer />
->>>>>>> main
             </div>
         </div>
     )
